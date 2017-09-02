699fd66908edc4871e5d0367dbd61dba0f4a0a32
pierobot <Xv24xt@airmail.cc>
2017-09-02 12:11:18 -0500

    rawurlencode archive names
    This fixes a bug in which Laravel would not correctly parse the URL into
    parameters for routes.

--- resources/views/manga/information.blade.php
+++ resources/views/manga/information.blade.php
@@ -3,16 +3,16 @@
 @section ('content')
 
 <div class="panel panel-default">
-    
+
     <div class="panel-heading">
         <h4 class="panel-title">{{ $name }}</h4>
     </div>
-    
+
     <div class="panel-body">
 
         {{ Html::image(URL::action('MangaController@thumbnail', [$id]), '', ['class' => 'information-img center-block']) }}
         <hr>
-        <!-- 
+        <!--
             other preview thumbnails here ?
         -->
 
@@ -21,15 +21,15 @@
             <li class="active"><a href="#information-content" data-toggle="tab"><span class="glyphicon glyphicon-info-sign"></span> Information</a></li>
             <li><a href="#files-content" data-toggle="tab"><span class="glyphicon glyphicon-folder-open"></span> Files</a></li>
             <li><a href="#edit-content" data-toggle="tab"><span class="glyphicon glyphicon-pencil"></span> Edit</a></li>
-            
+
         </ul>
 
         <div class="tab-content">
 
             <div class="tab-pane active" id="information-content">
-               
+
                 <ul class="list-group">
-                    
+
                     <li class="list-group-item">
                         <h4><span class="glyphicon glyphicon-info-sign"></span> Description</h4>
                     @if ($description != null)
@@ -110,7 +110,7 @@
                 @if ($archives != null)
                     @foreach ($archives as $archive)
                     <li class="list-group-item">
-                        <a href="{{ \Config::get('mangapie.app_url') }}/reader/{{ $id }}/{{ $archive['name'] }}/1">
+                        <a href="{{ \Config::get('mangapie.app_url') }}/reader/{{ $id }}/{{ rawurlencode($archive['name']) }}/1">
                         {{ $archive['name'] }}
                         </a>
                     </li>
