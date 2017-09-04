0ff94114a132b46f793ec99c68d76b19d15f02a9
pierobot <pierobot@users.noreply.github.com>
2017-09-04 15:51:38 -0500

    Add edit password in user/settings.blade.php
    The functionality to edit a user was originally in the admin page, but a
    user should be able to change their own password. It's now in
    user/settings.blade.php

--- app/Http/Controllers/UserSettingsController.php
+++ app/Http/Controllers/UserSettingsController.php
@@ -25,16 +25,36 @@ class UserSettingsController extends Controller
     public function update(Request $request)
     {
         $validator = \Validator::make($request->all(), [
-            'theme' => 'required|regex:/\w+\/\w+/'
+            'old-password' => 'string',
+            'new-password' => 'string',
+            'confirm-password' => 'same:new-password',
+            'theme' => 'regex:/\w+\/\w+/',
+            'action' => 'required|string'
         ]);
 
         if ($validator->fails() === true) {
             return \Redirect::action('UserSettingsController@index')->withErrors($validator, 'update');
         }
 
-        $theme = \Input::get('theme');
         $user = \Auth::user();
-        $user->setTheme($theme);
+        $action = \Input::get('action');
+        if ($action == 'updatepassword') {
+
+            // make sure the old password matches the current one
+            if (\Hash::check(\Input::get('old-password'), $user->getPassword())) {
+
+                $user->setPassword(\Hash::make(\Input::get('new-password')));
+
+                \Session::flash('edit-alert-success', 'Successfully updated password.');
+            }
+        } elseif ($action == 'updatetheme') {
+
+            $theme = \Input::get('theme');
+
+            $user->setTheme($theme);
+
+            \Session::flash('theme-alert-success', 'Successfully updated theme.');
+        }
 
         return \Redirect::action('UserSettingsController@index');
     }
