50249b1626fb1c70f009915603c9da8f83ccae62
pierobot <pierobot@users.noreply.github.com>
2017-09-17 13:18:40 -0500

    Add artisan command mangapie:scan
    This command can be used with cron to automatically update your
    libraries.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        Commands\Scan::class
     ];
 
     /**
@@ -24,8 +24,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule->command('mangapie:scan')
+                 ->everyFiveMinutes();
     }
 
     /**
