01f9e2324508f5a88e3cf66019c43b68b9c34311
pierobot <Xv24xt@airmail.cc>
2017-09-24 14:58:34 -0500

    Lockout users after 5 failed auth attempts.

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class LoginController extends Controller
 {
-    //
-    public function index() {
+    use AuthenticatesUsers;
+
+    protected $lockoutTime = 60 * 60 * 3;
+    protected $maxLoginAttempts = 5;
+
+    public function index()
+    {
         // if the user is already logged in then redirect to index
         if (\Auth::id() != null)
             return \Redirect::action('MangaController@index');
@@ -15,16 +22,28 @@ class LoginController extends Controller
         return view('login');
     }
 
-    public function login() {
+    public function login(Request $request)
+    {
+        if ($this->hasTooManyLoginAttempts($request)) {
+
+            $this->fireLockoutEvent($request);
+            \Session::flash('login-failure', 'You have been locked out. Try again in 3 hours.');
+
+            return view('login');
+        }
+
         $username = \Input::get('username');
         $password = \Input::get('password');
 
         if (\Auth::attempt(['name' => $username, 'password' => $password]) == false) {
 
+            $this->incrementLoginAttempts($request);
             \Session::flash('login-failure', 'You have given invalid credentials. Please try again.');
 
             return view('login');
         }
+
+        $this->clearLoginAttempts($request);
 
         return \Redirect::action('MangaController@index');
     }
