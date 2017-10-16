2ef5e064e609b7afe4cdb532755de2dde20b829a
pierobot <pierobot@users.noreply.github.com>
2017-10-15 20:34:26 -0500

    Move edit content to its own page from information.blade.php
    This commit also removes the usage of a main panel in
    manga/information.blade.php.
    
    manga/information.blade.php now has a navbar button that leads to
    manga/edit.blade.php
    
    Added ReaderController to handle the edit page.
    Added /edit route to routes/web.php

--- resources/views/manga/information.blade.php
+++ resources/views/manga/information.blade.php
@@ -8,15 +8,18 @@
     <link href="{{ URL::to('/public/css/manga/information.css') }}" rel="stylesheet">
 @endsection
 
+@section ('custom_navbar_right')
+    @if (\Auth::user()->isAdmin() || \Auth::user()->isMaintainer())
+        <li class="clickable navbar-link"><a href="{{ URL::action('MangaEditController@index', [$id]) }}"><span class="glyphicon glyphicon-pencil"></span> Edit</a></li>
+    @endif
+@endsection
+
 @section ('content')
-
-<div class="panel panel-default">
-
-    <div class="panel-heading">
-        <h4 class="panel-title">{{ $name }}</h4>
-    </div>
-
-    <div class="panel-body">
+        {{--
+            Only make the title visible on screens that are not xs (phones)
+            I don't think it looks very good on xs screens. Maybe there's a way to improve this?
+        --}}
+        <h2 class="visible-sm visible-md visible-lg visible-xl text-center">Information &middot; {{ $name }}</h2>
 
         @if ($errors->update->count() > 0)
             <div class="alert alert-danger">
@@ -36,13 +39,8 @@
         <hr>
 
         <ul class="nav nav-tabs">
-
             <li class="active"><a href="#information-content" data-toggle="tab"><span class="glyphicon glyphicon-info-sign"></span> Information</a></li>
             <li><a href="#files-content" data-toggle="tab"><span class="glyphicon glyphicon-folder-open"></span> Files</a></li>
-            @if (\Auth::user()->isAdmin() == true || \Auth::user()->isMaintainer() == true)
-            <li><a href="#edit-content" data-toggle="tab"><span class="glyphicon glyphicon-pencil"></span> Edit</a></li>
-            @endif
-
         </ul>
 
         <div class="tab-content">
@@ -169,92 +167,6 @@
                     </li>
                 </div>
             </div>
-
-            @if (\Auth::user()->isAdmin() == true || \Auth::user()->isMaintainer() == true)
-            <div class="tab-pane" id="edit-content">
-
-                <ul class="list-group">
-                    <li class="list-group-item">
-
-                        <h4>Mangaupdates</h4>
-
-                        {{ Form::open(['action' => 'MangaInformationController@update']) }}
-
-                            {{ Form::hidden('id', $id) }}
-
-                            <div class="input-group">
-
-                            {{ Form::label('id:', '', ['for' => 'mu_id']) }}
-                            @if ($mu_id != null)
-                                {{ Form::text('mu_id', '', ['class' => 'form-control', 'placeholder' => $mu_id]) }}
-                            @else
-                                {{ Form::text('mu_id', '', ['class' => 'form-control']) }}
-                            @endif
-
-                            </div>
-                            <br>
-                            {{ Form::submit('Update', ['class' => 'btn btn-success', 'id' => 'action', 'name' => 'action', 'value' => 'update']) }}
-
-                        {{ Form::close() }}
-                    </li>
-
-                </ul>
-
-                <li class="list-group-item">
-                    <h4><span class="glyphicon glyphicon-picture"></span>&nbsp; Cover</h4>
-
-                    @if (empty($archives) === false)
-
-                        @foreach ($archives as $archive_index => $archive)
-                            <div class="panel-group">
-                            <div class="panel panel-default">
-                                <div class="panel-heading">
-                                    <h3 class="panel-title">
-                                        <a data-toggle="collapse" href="#{{ $archive_index }}">{{ $archive['name'] }}</a>
-                                    </h3>
-                                </div>
-                                <div id="{{ $archive_index }}" class="panel-collapse collapse">
-                                    <div class="panel-body">
-                                        <div class="row">
-                                        @for ($i = 1; $i <= 4; $i++)
-                                            <div class="col-lg-2 col-sm-4 col-xs-6 set-cover thumbnail">
-                                                {{ Form::open(['action' => 'ThumbnailController@update'], [$id]) }}
-
-                                                    {{ Form::hidden('id', $id) }}
-                                                    {{ Form::hidden('archive_name', $archive['name']) }}
-                                                    {{ Form::hidden('page', $i) }}
-
-                                                    <div>
-                                                        {{ Html::image(URL::action('ThumbnailController@small', [
-                                                                           $id,
-                                                                           rawurlencode($archive['name']),
-                                                                           $i]), null, ['class' => 'center-block'])
-                                                        }}
-                                                    </div>
-
-                                                    <h4>
-                                                        {{ Form::submit('Set', ['class' => 'btn btn-success center-block']) }}
-                                                    </h4>
-
-                                                {{ Form::close() }}
-                                            </div>
-                                        @endfor
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            </div>
-
-                        @endforeach
-
-                    @endif
-                </li>
-
-            </div>
-            @endif
-
-        </div>
-
     </div>
 </div>
 
