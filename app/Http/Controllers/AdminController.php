19107ff268a1542ab723118dd74df7ab47224c72
pierobot <Xv24xt@airmail.cc>
2017-10-28 15:17:56 -0500

    Move user management from admin controller to user controller.

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\AdminCreateUserRequest;
-use App\Http\Requests\AdminDeleteUserRequest;
-use App\Http\Requests\AdminEditUserRequest;
+use App\Http\Requests\UserCreateRequest;
+use App\Http\Requests\UserDeleteRequest;
+use App\Http\Requests\UserEditRequest;
 use Illuminate\Http\Request;
 
 use \App\Library;
@@ -51,58 +51,5 @@ class AdminController extends Controller
         $libraries = Library::all();
 
         return view('admin.libraries', compact('libraries'));
-    }
-
-    public function createUser(AdminCreateUserRequest $request)
-    {
-        // create the user
-        $user = User::create([
-            'name' => \Input::get('name'),
-            'email' => \Input::get('email'),
-            'password' => \Hash::make(\Input::get('password')),
-            'admin' => \Input::get('admin') == null ? false : \Input::get('admin'),
-            'maintainer' => \Input::get('maintainer') == null ? false : \Input::get('maintainer')
-        ]);
-
-        if ($user != null) {
-            // create the privileges for each library
-            foreach (\Input::get('libraries') as $library_id) {
-                if (Library::find($library_id) == null)
-                    continue;
-
-                LibraryPrivilege::create([
-                    'user_id' => $user->getId(),
-                    'library_id' => $library_id
-                ]);
-            }
-
-            \Session::flash('success', 'User was successfully created!');
-        }
-
-        return \Redirect::action('AdminController@users');
-    }
-
-    public function editUser(AdminEditUserRequest $request)
-    {
-        $user = User::where('name', '=', \Input::get('old-name'))->first();
-        if ($user != null) {
-            $user->setName(\Input::get('new-name'));
-
-            \Session::flash('success', 'User was successfully edited!');
-        }
-
-        return \Redirect::action('AdminController@users');
-    }
-
-    public function deleteUser(AdminDeleteUserRequest $request)
-    {
-        $user = User::where('name', '=', \Input::get('name'))->first();
-        if ($user != null) {
-            $user->forceDelete();
-
-            \Session::flash('success', 'User was successfully deleted!');
-        }
-
-        return \Redirect::action('AdminController@users');
     }
 }
