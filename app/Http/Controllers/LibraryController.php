40bea5657ea203faa6ba335a81b9f2154b08fa9e
pierobot <Xv24xt@airmail.cc>
2017-10-28 02:42:57 -0500

    Refactor requests for library management.
    Replace checkmarks and one large form in favor of individual refresh and
    delete buttons which are individual forms.

--- app/Http/Controllers/LibraryController.php
+++ app/Http/Controllers/LibraryController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LibraryCreateRequest;
+use App\Http\Requests\LibraryDeleteRequest;
+use App\Http\Requests\LibraryUpdateRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Input;
 
@@ -14,82 +17,16 @@ class LibraryController extends Controller
         $this->middleware('auth');
     }
 
-    public function update(Request $request)
+    public function create(LibraryCreateRequest $request)
     {
-        // Ensure the form data is valid
-        $validator = \Validator::make($request->all(), [
-            'action' => 'required|string|max:10',
-            'ids'=> 'required|array'
-        ]);
-
-        if ($validator->fails())
-            return \Redirect::action('AdminController@libraries')->withErrors($validator, 'update');
-        
-        $action = \Input::get('action');
-        $libraries = Library::where('id', '=', \Input::get('ids'))->get();
-
-        if ($libraries == null) {
-
-            \Session::flash('library-update-failure', 'Unable to find the selected libraries.');
-
-            return \Redirect::action('AdminController@libraries');
-        }
-
-        // Rescan or delete the requested libraries
-        foreach ($libraries as $library) {
-
-            if ($action == 'Update') {
-
-                $library->scan();
-            } else if ($action == 'Delete') {
-
-                // delete the library and the manga it holds (forceDelete overriden)
-                $library->forceDelete();
-            } else {
-
-                \Session::flash('library-update-failure', 'Got invalid action.');
-
-                \Redirect::action('AdminController@libraries');
-            }
-        }
-
-        \Session::flash('library-update-success', 'The selected libraries were successfully ' . $action . 'd');
-
-        return \Redirect::action('AdminController@libraries');
-    }
-
-    public function create(Request $request)
-    {
-        if (\Auth::user()->isAdmin() == false)
-            return view('errors.403');
-
-        $validator = \Validator::make($request->all(), [
-            'name' => 'required|string|max:255',
-            'path' => 'required|string'
-        ]);
-
-        if ($validator->fails())
-            return \Redirect::action('AdminController@libraries')->withErrors($validator, 'create');
-
         $name = \Input::get('name');
         $path = \Input::get('path');
 
         // ensure we have a valid path
-        if (is_dir($request->path) == false) {
-
-            \Session::flash('library-create-failure', "'" . $path . "'" . ' does not exist');
-
-            return \Redirect::action('AdminController@libraries');
-        }
-
-        // ensure the name nor path match one that already axists
-        $library = Library::where('name', '=', $name)->first();
-
-        if ($library != null) {
-
-            \Session::flash('library-create-failure', 'A library with that name already exists.');
-
-            return \Redirect::action('AdminController@libraries');
+        if (is_dir($path) == false) {
+            return \Redirect::action('AdminController@libraries')->withErrors([
+                'library' => "'" . $path . "'" . ' does not exist'
+            ]);
         }
 
         // create the library
@@ -98,14 +35,40 @@ class LibraryController extends Controller
             'path' => $path,
         ]);
 
-        if ($library != null) {
-
-            $library->scan();
-
-            \Session::flash('library-create-success', 'Library was successfully created.');
-        } else {
-            \Session::flash('library-create-failure', 'Unable to create database row.');
+        if ($library == null) {
+            return \Redirect::action('AdminController@libraries')->withErrors([
+                'library' => 'Unable to create library.'
+            ]);
         }
+
+        // scan and populate the library
+        $library->scan();
+
+        \Session::flash('success', 'Library was successfully created.');
+
+        return \Redirect::action('AdminController@libraries');
+    }
+
+    public function update(LibraryUpdateRequest $request)
+    {
+        $id = \Input::get('id');
+
+        $library = Library::findOrFail($id);
+        $library->scan();
+
+        \Session::flash('success', 'The selected libraries were successfully updated.');
+
+        return \Redirect::action('AdminController@libraries');
+    }
+
+    public function delete(LibraryDeleteRequest $request)
+    {
+        $id = \Input::get('id');
+
+        $library = Library::findOrFail($id);
+        $library->forceDelete();
+
+        \Session::flash('success', 'The selected libraries were successfully deleted.');
 
         return \Redirect::action('AdminController@libraries');
     }
