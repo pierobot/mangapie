0e59a20695a71597d0587f39cec22d9170a880a2
pierobot <pierobot@users.noreply.github.com>
2017-11-03 15:43:55 -0500

    Update UI for admin pages
    Should be better... I hope. :^)

--- resources/views/admin/users.blade.php
+++ resources/views/admin/users.blade.php
@@ -9,97 +9,102 @@
     @include ('shared.success')
     @include ('shared.errors')
 
-    <ul class="nav nav-tabs">
-        <li class="active"><a href="#create-user-content" data-toggle="tab"><span class="glyphicon glyphicon-plus"></span> Create</a></li>
-        <li><a href="#delete-user-content" data-toggle="tab"><span class="glyphicon glyphicon-trash"></span> Delete</a></li>
-    </ul>
-
-    <div class="tab-content">
-        <div class="tab-pane active" id="create-user-content">
-            <ul class="list-group">
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <div class="panel-title">
+                <span class="glyphicon glyphicon-plus"></span> Create
+            </div>
+        </div>
+        <div class="panel-content">
+            <div class="panel-body">
                 {{ Form::open(['action' => 'UserController@create']) }}
 
-                <li class="list-group-item">
+                <div class="col-md-4">
                     <h4>Information</h4>
-
+                    <hr>
                     <div class="row">
-                        <div class="form-group col-xs-12 col-lg-3">
-                        {{ Form::label('name:', null, ['for' => 'name']) }}
-                        {{ Form::text('name', null, ['class' => 'form-control', 'placeholder' => 'Enter name here...']) }}
+                        <div class="form-group col-xs-12">
+                            {{ Form::label('name:', null, ['for' => 'name']) }}
+                            {{ Form::text('name', null, ['class' => 'form-control', 'placeholder' => 'Enter name here...']) }}
 
-                        {{ Form::label('email:', null, ['for' => 'email']) }}
-                        {{ Form::text('email', null, ['class' => 'form-control', 'placeholder' => 'Enter e-mail here...']) }}
+                            {{ Form::label('email:', null, ['for' => 'email']) }}
+                            {{ Form::text('email', null, ['class' => 'form-control', 'placeholder' => 'Enter e-mail here...']) }}
 
-                        {{ Form::label('password:', null, ['for' => 'password']) }}
-                        {{-- Form::password doesn't seem to display properly? hardcoded html for now. --}}
+                            {{ Form::label('password:', null, ['for' => 'password']) }}
+                            {{-- Form::password doesn't seem to display properly? hardcoded html for now. --}}
                             <input name="password" id="password" type="password" class="form-control" placeholder="Enter password here...">
                         </div>
+
                     </div>
-                </li>
+                </div>
 
-                <li class="list-group-item">
+                <div class="col-md-4">
                     <h4>Libraries</h4>
-
+                    <hr>
                     <div class="row">
-                        <div class="form-group col-xs-12 col-lg-6">
-                            @foreach ($libraries as $library)
-
+                        @foreach ($libraries as $library)
+                            <div class="form-group col-xs-12 col-md-6">
                                 {{ Form::checkbox('libraries[]', $library->getId(), ['class' => 'form-control']) }}
                                 {{ Form::label($library->getName(), null, ['for' => 'libraries[]', 'title' => $library->getPath()]) }}
-
-                                &nbsp;
-                            @endforeach
-                        </div>
+                            </div>
+                        @endforeach
                     </div>
-                </li>
+                </div>
 
-                <li class="list-group-item">
+                <div class="col-md-4">
                     <h4>Roles</h4>
-
+                    <hr>
                     <div class="row">
-                        <div class="form-group col-xs-12 col-lg-6">
+                        <div class="form-group col-xs-12 col-md-6">
                             {{ Form::checkbox('admin', 1, ['class' => 'form-control']) }}
                             {{ Form::label('admin-label', 'Admin', ['for' => 'admin']) }}
+                        </div>
 
+                        <div class="form-group col-xs-12 col-md-6">
                             {{ Form::checkbox('maintainer', 1, ['class' => 'form-control']) }}
                             {{ Form::label('maintainer-label', 'Maintainer', ['for' => 'maintainer']) }}
                         </div>
                     </div>
-                </li>
+                </div>
 
-                <li class="list-group-item">
-                    <div class="row">
-                        <div class="col-xs-4 col-lg-3">
-                            {{ Form::submit('Create', ['class' => 'btn btn-success']) }}
-                        </div>
+                <div class="col-xs-12">
+                    <div class="form-group">
+                        {{ Form::submit('Create', ['class' => 'btn btn-success']) }}
                     </div>
-                </li>
+                </div>
 
                 {{ Form::close() }}
-            </ul>
+            </div>
         </div>
+    </div>
 
-        <div class="tab-pane" id="delete-user-content">
-            <ul class="list-group">
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <div class="panel-title">
+                <span class="glyphicon glyphicon-trash"></span> Delete
+            </div>
+        </div>
+        <div class="panel-content">
+            <div class="panel-body">
                 {{ Form::open(['action' => 'UserController@delete']) }}
 
-                <li class="list-group-item">
+                <div class="col-xs-12">
                     <div class="row">
-                        <div class="form-group col-xs-12 col-lg-3">
+                        <div class="form-group col-xs-12 col-md-4">
                             {{ Form::label('name:', null, ['for' => 'name']) }}
                             {{ Form::text('name', null, ['class' => 'form-control', 'placeholder' => 'Enter name here...']) }}
                         </div>
                     </div>
+                </div>
 
-                    <div class="row">
-                        <div class="col-xs-4 col-lg-3">
-                            {{ Form::submit('Delete', ['class' => 'btn btn-danger']) }}
-                        </div>
+                <div class="col-xs-12">
+                    <div class="form-group">
+                        {{ Form::submit('Delete', ['class' => 'btn btn-danger']) }}
                     </div>
-                </li>
+                </div>
 
                 {{ Form::close() }}
-            </ul>
+            </div>
         </div>
     </div>
 @endsection
