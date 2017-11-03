ec2afcb2ed511981a552a8608c6015bd2c410468
pierobot <pierobot@users.noreply.github.com>
2017-11-03 16:22:53 -0500

    Update UI for settings page.

--- resources/views/user/settings.blade.php
+++ resources/views/user/settings.blade.php
@@ -10,87 +10,82 @@
     @include ('shared.success')
     @include ('shared.errors')
 
-    <div class="panel-body">
-
-        <ul class="nav nav-tabs">
-            <li class="active"><a href="#edit-user-content" data-toggle="tab"><span class="glyphicon glyphicon-pencil"></span> Edit</a></li>
-            <li><a href="#edit-theme-content" data-toggle="tab"><span class="glyphicon glyphicon-pencil"></span> Themes</a>
-            </li>
-        </ul>
-
-        <div class="tab-content">
-            <div class="tab-pane active" id="edit-user-content">
-
-                <ul class="list-group">
-
-                    {{ Form::open(['action' => 'UserSettingsController@update']) }}
-
-                    <li class="list-group-item">
-                        <div class="row">
-                            <div class="form-group col-xs-12 col-lg-3">
-                                {{ Form::hidden('action', 'password.update') }}
-                                {{ Form::label('old password:', null, ['for' => 'old-password']) }}
-                                <input name="old-password" id="old-password" type="password" class="form-control"
-                                       placeholder="Enter old password here...">
-
-                                {{ Form::label('new password:', null, ['for' => 'new-password']) }}
-                                <input name="new-password" id="new-password" type="password" class="form-control"
-                                       placeholder="Enter new password here...">
-
-                                {{ Form::label('confirm password:', null, ['for' => 'confirm-password']) }}
-                                <input name="confirm-password" id="confirm-password" type="password"
-                                       class="form-control" placeholder="Confirm new password here...">
-                            </div>
-                        </div>
-
-                        <div class="row">
-                            <div class="col-xs-4 col-lg-3">
-                                {{ Form::submit('Save', ['class' => 'btn btn-warning']) }}
-                            </div>
-                        </div>
-                    </li>
-
-                    {{ Form::close() }}
-                </ul>
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <div class="panel-title">
+                <span class="glyphicon glyphicon-pencil"></span>&nbsp;Personal
             </div>
+        </div>
+        <div class="panel-content">
+            <div class="panel-body">
+                {{ Form::open(['action' => 'UserSettingsController@update']) }}
 
-            <div class="tab-pane" id="edit-theme-content">
-                <ul class="list-group">
-                    <li class="list-group-item">
+                <div class="col-xs-12 col-md-4">
+                    <h4>Password</h4>
+                    <hr>
+                    <div class="form-group">
+                        {{ Form::hidden('action', 'password.update') }}
+                        {{ Form::label('old password:', null, ['for' => 'old-password']) }}
+                        <input name="old-password" id="old-password" type="password" class="form-control"
+                               placeholder="Enter old password here...">
 
-                        {{ Form::open(['action' => 'UserSettingsController@update']) }}
+                        {{ Form::label('new password:', null, ['for' => 'new-password']) }}
+                        <input name="new-password" id="new-password" type="password" class="form-control"
+                               placeholder="Enter new password here...">
 
-                        <div class="row">
-                            <div class="form-group col-xs-12 col-lg-3">
+                        {{ Form::label('confirm password:', null, ['for' => 'confirm-password']) }}
+                        <input name="confirm-password" id="confirm-password" type="password"
+                               class="form-control" placeholder="Confirm new password here...">
+                    </div>
+                </div>
 
-                                {{ Form::hidden('action', 'theme.update') }}
-                                {{ Form::label('theme:', null, ['for' => 'theme']) }}
+                <div class="col-xs-12">
+                    <div class="form-group">
+                        {{ Form::submit('Save', ['class' => 'btn btn-warning']) }}
+                    </div>
+                </div>
 
-                                <select name="theme" class="form-control">
-                                    <option disabled="disabled" selected="selected">{{ $current_theme }}</option>
-                                    @foreach ($theme_collections as $collection_name => $theme)
-                                        <optgroup label="{{ $collection_name }}">
+                {{ Form::close() }}
+            </div>
+        </div>
+    </div>
 
-                                            @foreach ($theme as $theme_name => $theme_path)
-                                                <option value="{{ $collection_name . '/' . $theme_name }}">{{ $theme_name }}</option>
-                                            @endforeach
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <div class="panel-title">
+                <span class="glyphicon glyphicon-pencil"></span>&nbsp;Themes
+            </div>
+        </div>
+        <div class="panel-content">
+            <div class="panel-body">
+                {{ Form::open(['action' => 'UserSettingsController@update']) }}
 
-                                        </optgroup>
-                                    @endforeach
-                                </select>
+                    <div class="col-xs-12 col-md-4">
+                        <div class="form-group ">
+                            {{ Form::hidden('action', 'theme.update') }}
+                            {{ Form::label('theme:', null, ['for' => 'theme']) }}
 
-                            </div>
+                            <select name="theme" class="form-control">
+                                <option disabled="disabled" selected="selected">{{ $current_theme }}</option>
+                                @foreach ($theme_collections as $collection_name => $theme)
+                                    <optgroup label="{{ $collection_name }}">
+
+                                        @foreach ($theme as $theme_name => $theme_path)
+                                            <option value="{{ $collection_name . '/' . $theme_name }}">{{ $theme_name }}</option>@endforeach
+
+                                    </optgroup>
+                                @endforeach
+                            </select>
                         </div>
+                    </div>
 
-                        <div class="row">
-                            <div class="form-group col-xs-12 col-lg-3">
-                                {{ Form::submit('Save', ['class' => 'btn btn-success']) }}
-                            </div>
+                    <div class="col-xs-12">
+                        <div class="form-group">
+                            {{ Form::submit('Save', ['class' => 'btn btn-success']) }}
                         </div>
+                    </div>
 
-                        {{ Form::close() }}
-                    </li>
-                </ul>
+                {{ Form::close() }}
             </div>
         </div>
     </div>
