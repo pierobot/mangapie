d71985a0b09209841ae72c8c43bb8dda9bb44a30
pierobot <pierobot@users.noreply.github.com>
2017-11-03 16:00:11 -0500

    Make success alerts dismissable.

--- resources/views/shared/success.blade.php
+++ resources/views/shared/success.blade.php
@@ -1,5 +1,8 @@
 @if (\Session::has('success'))
-    <div class="alert alert-success">
+    <div class="alert alert-success alert-dismissable">
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
         <span class="glyphicon glyphicon-ok"></span>&nbsp; {{ \Session::get('success') }}
     </div>
 @endif
