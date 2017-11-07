5a371f1d700baa10d74a81cc213adfe34e373842
pierobot <pierobot@users.noreply.github.com>
2017-11-06 19:35:25 -0600

    Browse & delete users from admin page

--- resources/views/admin/users.blade.php
+++ resources/views/admin/users.blade.php
@@ -12,29 +12,26 @@
     <div class="panel panel-default">
         <div class="panel-heading">
             <div class="panel-title">
-                <span class="glyphicon glyphicon-plus"></span> Create
+                <span class="glyphicon glyphicon-plus"></span>&nbsp;Create
             </div>
         </div>
-        <div class="panel-content">
-            <div class="panel-body">
+        <div class="panel-body">
+            <div class="row">
                 {{ Form::open(['action' => 'UserController@create']) }}
 
                 <div class="col-md-4">
                     <h4>Information</h4>
                     <hr>
-                    <div class="row">
-                        <div class="form-group col-xs-12">
-                            {{ Form::label('name:', null, ['for' => 'name']) }}
-                            {{ Form::text('name', null, ['class' => 'form-control', 'placeholder' => 'Enter name here...']) }}
+                    <div class="form-group">
+                        {{ Form::label('name:', null, ['for' => 'name']) }}
+                        {{ Form::text('name', null, ['class' => 'form-control', 'placeholder' => 'Enter name here...']) }}
 
-                            {{ Form::label('email:', null, ['for' => 'email']) }}
-                            {{ Form::text('email', null, ['class' => 'form-control', 'placeholder' => 'Enter e-mail here...']) }}
+                        {{ Form::label('email:', null, ['for' => 'email']) }}
+                        {{ Form::text('email', null, ['class' => 'form-control', 'placeholder' => 'Enter e-mail here...']) }}
 
-                            {{ Form::label('password:', null, ['for' => 'password']) }}
-                            {{-- Form::password doesn't seem to display properly? hardcoded html for now. --}}
-                            <input name="password" id="password" type="password" class="form-control" placeholder="Enter password here...">
-                        </div>
-
+                        {{ Form::label('password:', null, ['for' => 'password']) }}
+                        {{-- Form::password doesn't seem to display properly? hardcoded html for now. --}}
+                        <input name="password" id="password" type="password" class="form-control" placeholder="Enter password here...">
                     </div>
                 </div>
 
@@ -81,29 +78,42 @@
     <div class="panel panel-default">
         <div class="panel-heading">
             <div class="panel-title">
-                <span class="glyphicon glyphicon-trash"></span> Delete
+                <span class="glyphicon glyphicon-th-list"></span>&nbsp;Browse
             </div>
         </div>
-        <div class="panel-content">
-            <div class="panel-body">
-                {{ Form::open(['action' => 'UserController@delete']) }}
-
+        <div class="panel-body">
+            <div class="row">
                 <div class="col-xs-12">
-                    <div class="row">
-                        <div class="form-group col-xs-12 col-md-4">
-                            {{ Form::label('name:', null, ['for' => 'name']) }}
-                            {{ Form::text('name', null, ['class' => 'form-control', 'placeholder' => 'Enter name here...']) }}
-                        </div>
-                    </div>
+                    <table class="table table-responsive table-hover">
+                        <thead>
+                            <tr>
+                                <th class="col-xs-1"></th>
+                                <th>Name</th>
+                                <th>E-mail</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                             @foreach ($users as $user)
+                                <tr>
+                                    <td>
+                                        {{ Form::open(['action' => 'UserController@delete']) }}
+                                        <button class="btn btn-danger" type="submit" title="Delete"
+                                                name="name" value="{{ $user->getName() }}">
+                                            <span class="glyphicon glyphicon-remove"></span>
+                                        </button>
+                                        {{ Form::close() }}
+                                    </td>
+                                    <td>
+                                        {{ $user->getName() }}
+                                    </td>
+                                    <td>
+                                        {{ $user->getEmail() }}
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
                 </div>
-
-                <div class="col-xs-12">
-                    <div class="form-group">
-                        {{ Form::submit('Delete', ['class' => 'btn btn-danger']) }}
-                    </div>
-                </div>
-
-                {{ Form::close() }}
             </div>
         </div>
     </div>
