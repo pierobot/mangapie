47c373ed3dd06eef80f23ca19fb51075e2b28b30
pierobot <Xv24xt@airmail.cc>
2018-03-28 20:35:37 -0500

    Replace 'Search' text with glyphicon-search.
    Separate repetitive elements into respective blade files.

---
+++ resources/views/shared/libraries.blade.php
@@ -0,0 +1,12 @@
+<li class="dropdown">
+    <a href="#" class="dropdown-toggle" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <span class="glyphicon glyphicon-book"></span>&nbsp;Libraries&nbsp;<span class="glyphicon glyphicon-chevron-down white"></span>
+    </a>
+    <ul class="dropdown-menu" style="color: black;">
+        @foreach ($libraries as $library)
+            <li>
+                <a href="{{ URL::action('HomeController@library', ['id' => $library->getId()]) }}">{{ $library->getName() }}</a>
+            <li>
+        @endforeach
+    </ul>
+</li>
