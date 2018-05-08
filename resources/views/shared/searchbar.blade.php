7bc4c86d18aab37b8bd1d3f2ab82a47ffc09296b
pierobot <Xv24xt@airmail.cc>
2018-05-07 23:06:24 -0500

    Place input-group-btn inside form-group for searchbar.

--- resources/views/shared/searchbar.blade.php
+++ resources/views/shared/searchbar.blade.php
@@ -6,17 +6,17 @@
             {{ Form::text('keywords', null, ['class' => 'form-control',
                                              'placeholder' => 'Quick search',
                                              'id' => 'autocomplete']) }}
-        </div>
-        <div class="input-group-btn">
-            <span type="submit" class="btn btn-primary">
-                <!--
-                    Without any text before the span, the button does not match the size of the input above.
-                    Adding the class form-control fixes that but results in a square button.
+            <div class="input-group-btn">
+                <span type="submit" class="btn btn-primary">
+                    <!--
+                        Without any text before the span, the button does not match the size of the input above.
+                        Adding the class form-control fixes that but results in a square button.
 
-                    Seems like just having any text resolves this so I'm using the zero-width space. *shrug*
-                -->
-                &#8203;<span class="glyphicon glyphicon-search"></span>
-            </span>
+                        Seems like just having any text resolves this so I'm using the zero-width space. *shrug*
+                    -->
+                    &#8203;<span class="glyphicon glyphicon-search"></span>
+                </span>
+            </div>
         </div>
     </div>
     {{ Form::close() }}
