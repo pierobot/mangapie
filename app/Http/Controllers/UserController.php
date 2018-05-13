773d500e0bdfa89cad828369c1493c8680ae853e
pierobot <Xv24xt@airmail.cc>
2018-05-12 21:55:38 -0500

    Add auth middleware to UserController (doh!)

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,7 +13,11 @@ use App\LibraryPrivilege;
 
 class UserController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
     }
