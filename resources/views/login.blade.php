76fa05445cafc2f5b821d6ce497f797357c361b5
pierobot <pierobot@users.noreply.github.com>
2018-10-21 20:41:54 -0500

    Update login view.

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,41 +5,45 @@
 @endsection
 
 @section ('content')
-    <div class="row pt-2">
-        <div class="col-lg-6 mx-auto">
-            <div class="card bg-transparent">
+    <div class="row mt-3 justify-content-center">
+        <div class="col-12 col-lg-8">
+            <div class="card">
                 <div class="card-header">
-                    <h4 class="card-title text-center">Login</h4>
+                    <h4 class="card-title text-center mb-0">Login</h4>
                 </div>
                 <div class="card-body">
                     {{ Form::open([ 'action' => 'LoginController@login']) }}
-                    <div class="form-row">
-                        <div class="col-12 col-md-6 mx-auto mb-3">
+                    <div class="form-row justify-content-center">
+                        <div class="col-12 col-xl-7 mb-3">
                             <div class="input-group">
                                 <div class="input-group-prepend" id="prepend-username">
-                                    <span class="input-group-text fa fa-user"></span>
+                                    <span class="input-group-text">
+                                        <span class="fa fa-user"></span>
+                                    </span>
                                 </div>
 
-                                {{ Form::text('username', null, ['class' => 'form-control', 'aria-describedby' => 'prepend-username']) }}
+                                <input type="text" class="form-control" id="username" name="username" title="Username" placeholder="Username">
                             </div>
                         </div>
                     </div>
 
-                    <div class="form-row">
-                        <div class="col-12 col-md-6 mx-auto mb-3">
+                    <div class="form-row justify-content-center">
+                        <div class="col-12 col-xl-7 mb-3">
                             <div class="input-group">
-                                <div class="input-group-prepend" id="prepend-password">
-                                    <span class="input-group-text fa fa-asterisk"></span>
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text">
+                                        <span class="fa fa-lock"></span>
+                                    </span>
                                 </div>
 
-                                <input name="password" id="password" type="password" class="form-control" aria-describedby="prepend-password">
+                                <input type="password" class="form-control" id="password" name="password" title="Password" placeholder="Password">
                             </div>
                         </div>
                     </div>
 
-                    <div class="form-row">
-                        <div class="col-12 text-center">
-                            <button class="btn bg-primary" type="submit">
+                    <div class="form-row justify-content-center">
+                        <div class="col-12 col-xl-7">
+                            <button class="btn btn-primary form-control" type="submit">
                                 <span class="fa fa-sign-in"></span>&nbsp; Login
                             </button>
                         </div>
@@ -52,8 +56,4 @@
             </div>
         </div>
     </div>
-@endsection
-
-@section ('scripts')
-
 @endsection
