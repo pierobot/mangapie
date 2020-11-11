1a51fc85d5e4dfe8aa47f385713834da9e28cf79
Kurim <kurim@me.com>
2020-11-11 12:37:01 +0100

    add notification count #212

--- resources/views/shared/notifications.blade.php
+++ resources/views/shared/notifications.blade.php
@@ -4,6 +4,9 @@
     <a class="ml-auto" href="{{ URL::action('NotificationController@index') }}" title="{{ $notifications->count() }} notification(s)">
         <button class="navbar-toggler @if ($notifications->count()) btn btn-outline-secondary @endif" type="button">
             <span class="fa fa-bell @if ($notifications->count()) text-warning @endif"></span>
+            @if ($notifications->count() > 0)
+            <span class="badge badge-pill badge-warning badge-notify">{{ $notifications->count() }}</span>
+			@endif
         </button>
     </a>
 @endauth
